31399c409c8cd34ae83542b75fd869726c6d5976
tera <tera.1999.kagayaku@ezweb.ne.jp>
2023-05-22 20:05:05 +0900

    add: mysql connect

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"go-echo-sample/model"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+func connect(c echo.Context) error {
+	db, _ := model.DB.DB()
+	defer db.Close()
+	err := db.Ping()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "DB接続失敗しました")
+	}
+
+	return c.String(http.StatusOK, "DB接続しました")
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", connect)
 	e.Logger.Fatal(e.Start(":8080"))
 }
